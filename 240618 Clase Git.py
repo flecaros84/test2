9002029240caf76a44d265b66347382431d93549
flecaros84 <fabi.lecaros@duocuc.cl>
2024-06-18 19:46:38 -0400

    Agregue unas líneas

--- 240618 Clase Git.py
+++ 240618 Clase Git.py
@@ -1,1 +1,10 @@
-print("Hola GIT y GitHub")
+print("Hola GIT y GitHub en       Fundamentos")
+#ls -a Listar archivos ocultos
+#git status Verificar estado de los archivos
+#git add . Agregar todos los archivos
+#git add "nombre_archivo.py" Agregar un archivo
+#git commit -m "nombre_archivo.py" "Mensaje" Guardar cambios
+#git commit --amend "nombre_archivo.py" Corregir mensaje del commit
+#para salir del terminal se apreta ESC y se escribe ":wq"
+#git config --global core.abbrev 10: Abreviar el nombre de los archivos
+#git log --oneline: Ver los commits de manera resumida
