1691bb059851708e6ad87768002c06e07220545a
flecaros84 <fabi.lecaros@duocuc.cl>
2024-06-18 20:01:36 -0400

    Esto se edito en la rama2

--- 240618 Clase Git.py
+++ 240618 Clase Git.py
@@ -1,10 +1,15 @@
-print("Hola GIT y GitHub en       Fundamentos")
+print("Hola GIT y GitHub en       Fundamentos rama2")
 #ls -a Listar archivos ocultos
 #git status Verificar estado de los archivos
 #git add . Agregar todos los archivos
 #git add "nombre_archivo.py" Agregar un archivo
-#git commit -m "nombre_archivo.py" "Mensaje" Guardar cambios
+#git commit -m "comentario" "nombre_archivo.py": Agregar comentario al archivo
 #git commit --amend "nombre_archivo.py" Corregir mensaje del commit
 #para salir del terminal se apreta ESC y se escribe ":wq"
 #git config --global core.abbrev 10: Abreviar el nombre de los archivos
 #git log --oneline: Ver los commits de manera resumida
+#git remote add origin https://github.com/flecaros84/test2.git : Agregar repositorio remoto
+#git clone url: Clonar repositorio
+#git push origin master: Subir cambios al repositorio
+#git branch: Ver las ramas
+#git switch -c "nombre_rama": Crear una rama
